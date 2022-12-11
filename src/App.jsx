d3b7ff463422349ecc92386e1c686742cfaa2490
tples <tmedia.produkcija@gmail.com>
2022-12-11 09:31:17 +0100

    Solution 2: position absolute

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
 import './App.css';
+import { useEffect, useState, useRef } from 'react';
 
 export default function App() {
    const [documentFields, setDocumentFields] = useState([]);
@@ -13,14 +13,12 @@ export default function App() {
    }, []);
 
    return (
-      <div>
+      <>
          {documentFields.map((field, index) => {
             const { comboboxExtras } = field.options;
             const { fieldType } = field;
-            let { width, height, borderWidth, borderColor, fontSize, borderStyle, fontStyle, fontColor, padding } = field.options.visualisation;
-
-            width = width * 1000;
-            height = height * 1000;
+            let { width, height, borderWidth, borderColor, fontSize, borderStyle, fontStyle, fontColor, padding, } = field.options.visualisation;
+            let { x, y } = field.options.visualisation.location;
 
             switch (borderStyle) {
                case 'DOT':
@@ -48,37 +46,40 @@ export default function App() {
 
             if (fieldType === 'COMBOBOX')
                return (
-                  <div key={index}>
-                     <label htmlFor="itemLines">Choose Item line </label>
-                     <select
-                        name="itemLines"
-                        id="itemLines"
-                        style={{
-                           width: width,
-                           height: height,
-                           border: `${borderWidth}px ${borderStyle} rgba(${borderColor})`,
-                           padding: padding,
-                           fontSize: fontSize,
-                           color: `rgba(${fontColor})`,
-                           fontWeight: fontStyle,
-                        }}
-                        defaultValue={comboboxExtras.defaultOptionKey}
-                     >
-                        {Object.entries(comboboxExtras.options).map((option, index) => (
-                           <option key={index} value={option[0]}>
-                              {option[1]}
-                           </option>
-                        ))}
-                     </select>
-                  </div>
+                  <select
+                     name="itemLines"
+                     id="itemLines"
+                     key={index}
+                     style={{
+                        position: 'absolute',
+                        zIndex: '999',
+                        top: `${y * 100}%`,
+                        left: `${x * 100}%`,
+
+                        width: `${width * 100}%`,
+                        height: `${height * 100}%`,
+                        border: `${borderWidth}px ${borderStyle} rgba(${borderColor})`,
+                        padding: padding,
+                        fontSize: fontSize,
+                        color: `rgba(${fontColor})`,
+                        fontWeight: fontStyle,
+                     }}
+                     defaultValue={comboboxExtras.defaultOptionKey}
+                  >
+                     {Object.entries(comboboxExtras.options).map((option, index) => (
+                        <option key={index} value={option[0]}>
+                           {option[1]}
+                        </option>
+                     ))}
+                  </select>
                );
          })}
 
          <object
-            style={{ height: '700px', width: '500px' }}
-            data="./pdf-test.pdf"
+            style={{ height: '100vh', width: '100vw', position: 'relative' }}
+            data="./pdf-blank.pdf#view=fit"
             type="application/pdf"
          ></object>
-      </div>
+      </>
    );
 }
