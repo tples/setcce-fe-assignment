85113c1e45f784dad7e96c3303ccc3132ca9fe36
tples <tmedia.produkcija@gmail.com>
2022-12-12 20:52:52 +0100

    react-pdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react';
-import { Document, Page,pdfjs } from 'react-pdf/dist/esm/entry.vite';
+import { Document, Page, pdfjs } from 'react-pdf/dist/esm/entry.vite';
+import { useWindowSize } from './hooks/useWindowSize';
+import './App.css'
 
 export default function App() {
-   pdfjs.GlobalWorkerOptions.workerSrc =`//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
    const [documentFields, setDocumentFields] = useState([]);
+   const size = useWindowSize();
 
    useEffect(() => {
       fetch('https://639335b5ab513e12c50722ff.mockapi.io/job')
@@ -14,70 +18,70 @@ export default function App() {
    }, []);
 
    return (
-      <div style={{background: 'grey'}}>
-         {documentFields.map((field, index) => {
-            const { comboboxExtras } = field.options;
-            const { fieldType } = field;
-            let { width, height, borderWidth, borderColor, fontSize, borderStyle, fontStyle, fontColor, padding, } = field.options.visualisation;
-            let { x, y } = field.options.visualisation.location;
+      <section style={{backgroundColor: '#ccc'}}>
 
-            switch (borderStyle) {
-               case 'DOT':
-                  borderStyle = 'dotted';
-                  break;
-               case 'DASH':
-                  borderStyle = 'dashed';
-                  break;
-               default:
-                  borderStyle = 'solid';
-                  break;
-            }
+         <div className="pdf-wrapper" >
+            {documentFields.map((field, index) => {
+               const { comboboxExtras } = field.options;
+               const { fieldType } = field;
+               let { width, height, borderWidth, borderColor, fontSize, borderStyle, fontStyle, fontColor, padding } = field.options.visualisation;
+               let { x, y } = field.options.visualisation.location;
+               switch (borderStyle) {
+                  case 'DOT':
+                     borderStyle = 'dotted';
+                     break;
+                  case 'DASH':
+                     borderStyle = 'dashed';
+                     break;
+                  default:
+                     borderStyle = 'solid';
+                     break;
+               }
+               switch (fontStyle) {
+                  case 'PLAIN':
+                     fontStyle = 'normal';
+                     break;
+                  case 'BOLD':
+                     fontStyle = 'bold';
+                     break;
+                  default:
+                     fontStyle = 'normal';
+                     break;
+               }
+               if (fieldType === 'COMBOBOX')
+                  return (
+                     <select
+                        name="itemLines"
+                        id="itemLines"
+                        key={index}
+                        style={{
+                           position: 'absolute',
+                           zIndex: '999',
+                           bottom: `${y * 100}%`,
+                           left: `${x * 100}%`,
+                           width: `${width * 100}%`,
+                           height: `${height * 100}%`,
+                           border: `${borderWidth}px ${borderStyle} rgba(${borderColor})`,
+                           padding: padding,
+                           fontSize: fontSize,
+                           color: `rgba(${fontColor})`,
+                           fontWeight: fontStyle,
+                        }}
+                        defaultValue={comboboxExtras.defaultOptionKey}
+                     >
+                        {Object.entries(comboboxExtras.options).map((option, index) => (
+                           <option key={index} value={option[0]}>
+                              {option[1]}
+                           </option>
+                        ))}
+                     </select>
+                  );
+            })}
+            <Document file="./pdf-test.pdf">
+               <Page height={size.height} pageNumber={1}></Page>
+            </Document>
+         </div>
 
-            switch (fontStyle) {
-               case 'PLAIN':
-                  fontStyle = 'normal';
-                  break;
-               case 'BOLD':
-                  fontStyle = 'bold';
-                  break;
-               default:
-                  fontStyle = 'normal';
-                  break;
-            }
-
-            if (fieldType === 'COMBOBOX')
-               return (
-                  <select
-                     name="itemLines"
-                     id="itemLines"
-                     key={index}
-                     style={{
-                        position: 'absolute',
-                        zIndex: '999',
-                        bottom: `${y * 100}%`,
-                        left: `${x * 100}%`,
-
-                        width: `${width * 100}%`,
-                        height: `${height * 100}%`,
-                        border: `${borderWidth}px ${borderStyle} rgba(${borderColor})`,
-                        padding: padding,
-                        fontSize: fontSize,
-                        color: `rgba(${fontColor})`,
-                        fontWeight: fontStyle,
-                     }}
-                     defaultValue={comboboxExtras.defaultOptionKey}
-                  >
-                     {Object.entries(comboboxExtras.options).map((option, index) => (
-                        <option key={index} value={option[0]}>
-                           {option[1]}
-                        </option>
-                     ))}
-                  </select>
-               );
-         })}
-         <Document file="./pdf-test.pdf">
-            <Page  pageNumber={1}></Page>
-         </Document>
-      </div>
+      </section>
    );
 }
